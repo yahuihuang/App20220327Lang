1be10b774f460194d21b0f263c336ae72833ca36
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 10:13:56 +0800

    設定語系檔

--- App20220327Lang/ViewController.swift
+++ App20220327Lang/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myLabel.text = NSLocalizedString("Hello", comment: "")
+        print(Locale.current)
     }
 
 
